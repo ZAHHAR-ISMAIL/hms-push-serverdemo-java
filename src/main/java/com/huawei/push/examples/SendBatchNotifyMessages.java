6a3b59c169e8d48a48222e465c9e2a0dc11c5e4f
zahhar ismail <zahhar.ismail@gmail.com>
2024-07-28 18:06:14 +0300

    update SendEach Example Code

--- src/main/java/com/huawei/push/examples/SendBatchNotifyMessages.java
+++ src/main/java/com/huawei/push/examples/SendBatchNotifyMessages.java
@@ -35,12 +35,6 @@ public class SendBatchNotifyMessages {
         HuaweiApp app = InitAppUtils.initializeApp();
         HuaweiMessaging huaweiMessaging = HuaweiMessaging.getInstance(app);
 
-//        String title1 = "title1";
-//        String message1 = "message1";
-//
-//        String title2 = "title2";
-//        String message2 = "message2";
-
         int id1 = 1;
         String title1 = "title1";
         String message1 = "message1";
@@ -83,9 +77,9 @@ public class SendBatchNotifyMessages {
 
             JSONObject multiLangKey = new JSONObject();
             JSONObject titleKey = new JSONObject();
-            titleKey.put("en",pn.getTitle());
+            titleKey.put("en", pn.getTitle());
             JSONObject bodyKey = new JSONObject();
-            bodyKey.put("en","My name is %s, I am from %s.");
+            bodyKey.put("en", "My name is %s, I am from %s.");
             multiLangKey.put("key1", titleKey);
             multiLangKey.put("key2", bodyKey);
 
@@ -139,79 +133,6 @@ public class SendBatchNotifyMessages {
             messages.add(message);
 
         }
-
-//        Notification notification_1 = Notification.builder().setTitle("sample title 1")
-//                .setBody("sample message body 1")
-//                .build();
-//
-//        Notification notification_2 = Notification.builder().setTitle("sample title 2")
-//                .setBody("sample message body 2")
-//                .build();
-//
-//        JSONObject multiLangKey = new JSONObject();
-//        JSONObject titleKey = new JSONObject();
-//        titleKey.put("en","helloMagic");
-//        JSONObject bodyKey = new JSONObject();
-//        bodyKey.put("en","My name is %s, I am from %s.");
-//        multiLangKey.put("key1", titleKey);
-//        multiLangKey.put("key2", bodyKey);
-//
-//        LightSettings lightSettings = LightSettings.builder().setColor(Color.builder().setAlpha(0f).setRed(0f).setBlue(1f).setGreen(1f).build())
-//                .setLightOnDuration("3.5")
-//                .setLightOffDuration("5S")
-//                .build();
-//
-//        AndroidNotification androidNotification = AndroidNotification.builder().setIcon("/raw/ic_launcher2")
-//                .setColor("#AACCDD")
-//                .setSound("/raw/shake")
-//                .setDefaultSound(true)
-//                .setTag("tagBoom")
-//                .setClickAction(ClickAction.builder().setType(2).setUrl("https://www.huawei.com").build())
-//                .setBodyLocKey("key2")
-//                .addBodyLocArgs("boy").addBodyLocArgs("dog")
-//                .setTitleLocKey("key1")
-//                .addTitleLocArgs("Girl").addTitleLocArgs("Cat")
-//                .setChannelId("Your Channel ID")
-//                .setNotifySummary("some summary")
-//                .setMultiLangkey(multiLangKey)
-//                .setStyle(1)
-//                .setBigTitle("Big Boom Title")
-//                .setBigBody("Big Boom Body")
-//                .setAutoClear(86400000)
-//                .setNotifyId(486)
-//                .setGroup("Group1")
-//                .setImportance(Importance.LOW.getValue())
-//                .setLightSettings(lightSettings)
-//                .setBadge(BadgeNotification.builder().setAddNum(1).setBadgeClass("Classic").build())
-//                .setVisibility(Visibility.PUBLIC.getValue())
-//                .setForegroundShow(true)
-//                .addInboxContent("content1").addInboxContent("content2").addInboxContent("content3").addInboxContent("content4").addInboxContent("content5")
-//                .addButton(Button.builder().setName("button1").setActionType(0).build())
-//                .addButton(Button.builder().setName("button2").setActionType(1).setIntentType(0).setIntent("https://com.huawei.hms.hmsdemo/deeplink").build())
-//                .addButton(Button.builder().setName("button3").setActionType(4).setData("your share link").build())
-//                .build();
-//
-//        AndroidConfig androidConfig = AndroidConfig.builder().setCollapseKey(-1)
-//                .setUrgency(Urgency.HIGH.getValue())
-//                .setTtl("10000s")
-//                .setBiTag("the_sample_bi_tag_for_receipt_service")
-//                .setNotification(androidNotification)
-//                .build();
-//
-//        Message message_1 = Message.builder().setNotification(notification_1)
-//                .setAndroidConfig(androidConfig)
-//                .addToken("IQAAAACy07ONAACUbGA8FAHZV7-6AECUwKUnXoERL-L7N2lhK631y89mxFBOvf10GW7kZDTrOMrBLA0p9wmVyfnpRBfVPbteOCgrjJyIX_4qe02uTw")
-//                .build();
-//
-//        Message message_2 = Message.builder().setNotification(notification_2)
-//                .setAndroidConfig(androidConfig)
-//                .addToken("IQAAAACy07ONAADq5Az0KdTm0ehBsYczBYtaeADmoHQXkd1TTI6sWahjWFgsN9VAi_YXMsvDR5Qc1WiYRXD_6teci1h-hCGQxtMKmN1_lv_InP2qPg")
-//                .build();
-//
-//        List<Message> messages = new ArrayList<>();
-//
-//        messages.add(message_1);
-//        messages.add(message_2);
 
         BatchResponse batchResponse = huaweiMessaging.sendEach(messages);
 
